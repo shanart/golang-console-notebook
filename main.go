88e21ff12393a4ca36c335457a4fc320c04ce863
shanart <shanin.x@gmail.com>
2022-01-22 00:29:11 +0200

    add logger

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
+func logger(mode *bool, message string) {
+	if *mode {
+		log.Println(message)
+	}
+}
+
 func main() {
+
 	f_get := flag.String("get", "", "Get note")
 	f_create := flag.String("create", "", "Create new note")
 	f_delete := flag.Int("delete", 0, "Delete note by id")
@@ -13,8 +22,21 @@ func main() {
 	f_text := flag.String("text", "", "Content. Require using -u/--update flag to get updated note id")
 	f_search := flag.String("search", "", "Search note")
 	f_init := flag.String("init", "", "Init database")
-
+	debug := flag.Bool("debug", false, "Debug mode")
 	flag.Parse()
+
+	if *debug {
+		f, err := os.OpenFile("debug.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		log.SetOutput(f)
+
+		logger(debug, "Application startup")
+	}
+
+	logger(debug, "Read flags")
 
 	fmt.Printf("%s", *f_get)
 	fmt.Printf("%s", *f_create)

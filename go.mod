38303bc0f1d599795b1c7a151f3b8e608b4e3b07
shanart <shanin.x@gmail.com>
2022-02-08 09:40:13 +0200

    add gocui

--- go.mod
+++ go.mod
@@ -8,8 +8,7 @@ require (
 )
 
 require (
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/jroimartin/gocui v0.5.0
 	github.com/mattn/go-sqlite3 v1.14.11 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,98 @@
 package main
 
 import (
-	"notebook/app"
-	"notebook/app/config"
+	"fmt"
+	"log"
+
+	"github.com/jroimartin/gocui"
+)
+
+var (
+	viewArr = []string{"sidebar", "editor"}
+	active  = 0
 )
 
 func main() {
-	config := config.GetConfig()
+	// config := config.GetConfig()
 
-	app := &app.App{}
-	app.Initialize(config)
-	app.Run()
+	// app := &app.App{}
+	// app.Initialize(config)
+	// app.Run()
+
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.Highlight = true
+	g.Cursor = true
+	g.SelFgColor = gocui.ColorGreen
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyTab, gocui.ModNone, nextView); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("sidebar", 0, 0, 30, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "Sidebar"
+		fmt.Fprintln(v, "Sidebar notes tree")
+	}
+
+	if v, err := g.SetView("editor", 31, 0, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "Note title"
+		v.Editable = true
+		v.Highlight = true
+		v.Wrap = true
+
+		fmt.Fprintln(v, "Note detail")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func setCurrentViewOnTop(g *gocui.Gui, name string) (*gocui.View, error) {
+	if _, err := g.SetCurrentView(name); err != nil {
+		return nil, err
+	}
+	return g.SetViewOnTop(name)
+}
+
+func nextView(g *gocui.Gui, v *gocui.View) error {
+	nextIndex := (active + 1) % len(viewArr)
+	name := viewArr[nextIndex]
+
+	if _, err := setCurrentViewOnTop(g, name); err != nil {
+		return err
+	}
+
+	if nextIndex == 0 {
+		g.Cursor = false
+	} else {
+		g.Cursor = true
+	}
+
+	active = nextIndex
+	return nil
 }
